37bf27711d88c7d446635dde1f9a753cf7cf9582
SamosMC <thesamosmc@gmail.com>
2022-06-06 14:06:29 -0300

    Um pouco sobre os tipos primitivos...

---
+++ 04-tipos/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	/*
+		Tipos de dados primitivos: int, string, float, bool
+		Tipos de dados compostos: slice, array, struct, map
+			* O ato de definir, estruturar, criar os tipos compostos é chamado de "composição"
+	*/
+
+	// até eu fiquei triste com esse exemplo :v
+}
